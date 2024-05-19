35671553abc009232f4a051d23dd18bc27e760de
Keegan <govenderkeegan33@gmail.com>
2024-05-19 17:15:22 +0200

    Connect backend

--- src/services/api-endpoints.ts
+++ src/services/api-endpoints.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8082',
+  //baseURL: 'http://localhost:8082',
+  baseURL: 'http://enhapp-dev-env.eba-pvtq6ayz.eu-north-1.elasticbeanstalk.com',
   headers: {
     'Content-Type': 'application/json',
   },
