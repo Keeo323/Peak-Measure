75919baa8f7c8b0bb7a2dd9ae6b66e600e61e099
Keegan <govenderkeegan33@gmail.com>
2024-05-19 18:25:59 +0200

    NGrok

--- src/services/api-endpoints.ts
+++ src/services/api-endpoints.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const apiClient = axios.create({
   //baseURL: 'http://localhost:8082',
-  baseURL: 'http://enhapp-dev-env.eba-pvtq6ayz.eu-north-1.elasticbeanstalk.com',
+  //baseURL: 'http://enhapp-dev-env.eba-pvtq6ayz.eu-north-1.elasticbeanstalk.com',
+  baseURL: 'https://40b5-2c0f-f528-41-feec-d169-3b53-c3de-c493.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json',
   },
